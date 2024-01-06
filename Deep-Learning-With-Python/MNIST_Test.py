e19c51525f6531e5c4cf134939020b91d1fd15be
Yves Agbre <yagbre@gmail.com>
2024-01-05 22:23:55 -0500

    Completed Test of MNIST_Test.py on 2021-10-20 15:51:00.000000

--- Deep-Learning-With-Python/MNIST_Test.py
+++ Deep-Learning-With-Python/MNIST_Test.py
@@ -1,3 +1,8 @@
+'''
+Practicing with the MNIST dataset using the examples from 
+Deep Learning with Python by Francois Chollet, Chapter 2
+'''
+
 from tensorflow.keras.datasets import mnist
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
@@ -24,6 +29,15 @@ model.fit(train_images, train_labels, epochs=5, batch_size=128)
 
 test_digits = test_images[0:10]
 predictions = model.predict(test_digits)
-predictions[0]
 
 print(f"\narray({predictions[0]})")
+
+print(f"\nargmax: {predictions[0].argmax()}")
+
+print(f"\npredictions[0][7]: {predictions[0][7]}")
+
+print(f"\ntests_labels[0]: {test_labels[0]}")
+
+# Evaluating the model on new data
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print(f"\ntest_acc: {test_acc}")
